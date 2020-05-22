0062bc5f3b875bcdd935ad5348e2088ad85b0697
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-05-22 15:52:14 -0500

    Crear calculadora

---
+++ Calculadora.py
@@ -0,0 +1,25 @@
+print("Calculadora\n")
+
+n1 = float(input("Ingresa el primer número: "))
+n2 = float(input("Ingresa el segundo número: "))
+
+op = input("Escoge una operación: (+, -, *, /, %):")
+
+
+def selector(n1, n2, op):
+    if op == '+':
+        return n1 + n2
+    elif op == '-':
+        return n1 - n2
+    elif op == '*':
+        return n1 * n2
+    elif op == '/':
+        return n1 / n2
+    elif op == '%':
+        return n1 % n2
+    else:
+        return 0
+
+
+r = selector(n1, n2, op[0])
+print("Resultado:", r)
