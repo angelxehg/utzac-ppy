cc7f73e7b2912208f49f6b43a3ebc1bb2602f076
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-05-27 23:24:33 -0500

    Juego de colores

--- Unidad 1/Datos.py
+++ Unidad 1/Datos.py
@@ -1,3 +1,5 @@
+from Paleta import Paleta
+
 print("Ingresa tus datos personales")
 nombre = input("Nombre: ")
 apellido = input("Apellido: ")
@@ -6,7 +8,7 @@ fecha = input("Fecha de nacimiento: ")
 ciudad = input("Ciudad de origen: ")
 
 print("\nDatos obtenidos:")
-print("Nombre completo:", nombre, apellido)
-print("Fecha de nacimiento:", fecha)
-print("Edad:", edad)
-print("Ciudad de origen:", ciudad)
+print("Nombre completo:", Paleta.AMARILLO, nombre, apellido, Paleta.CIERRE)
+print("Fecha de nacimiento:", Paleta.AMARILLO, fecha, Paleta.CIERRE)
+print("Edad:", Paleta.AMARILLO, edad, Paleta.CIERRE)
+print("Ciudad de origen:", Paleta.AMARILLO, ciudad, Paleta.CIERRE)
--- Unidad 1/Edad.py
+++ Unidad 1/Edad.py
@@ -1,4 +1,5 @@
 from datetime import date
+from Paleta import Paleta
 
 y = int(input("¿En qué año naciste? (YYYY): "))
 m = int(input("¿En qué mes naciste? (MM): "))
@@ -7,8 +8,8 @@ d = int(input("¿En qué dia naciste? (DD): "))
 td = date.today()
 
 if td.month > m:
-    print("Edad:", td.year - y)
+    print("Edad:", Paleta.AZUL, td.year - y, Paleta.CIERRE)
 elif td.month == m and td.day >= d:
-    print("Edad:", td.year - y)
+    print("Edad:", Paleta.AZUL, td.year - y, Paleta.CIERRE)
 else:
-    print("Edad:", td.year - y - 1)
+    print("Edad:", Paleta.AZUL, td.year - y - 1, Paleta.CIERRE)
