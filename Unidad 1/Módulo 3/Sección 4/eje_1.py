64f3108b9e136c44d0fae851cb9e4af3c07e001f
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-01 21:49:37 -0500

    Añadir ejercicios sección 4

---
+++ Unidad 1/Módulo 3/Sección 4/eje_1.py
@@ -0,0 +1,8 @@
+# Esta es una lista existente de números ocultos en el sombrero.
+listaSombrero = [1, 2, 3, 4, 5]
+
+nuevo_central = int(input("Ingrese un número para reemplazar el centro: "))
+del listaSombrero[4]
+print(len(listaSombrero))
+
+print(listaSombrero)
