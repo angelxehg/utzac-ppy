41ef5d54353388d04fc8c180d1819ae70813cf93
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-05 15:37:44 -0500

    Add colors

--- Unidad 1/Tupla.py
+++ Unidad 1/Tupla.py
@@ -1,34 +1,36 @@
 miTupla = (1, 10, 100, 1000)
 
-print("Elemento 0: ", miTupla[0])
-print("Elemento 1: ", miTupla[1])
-print("Elemento despues de 1: ", miTupla[1:])
-print("Elementos desde la pos -2 ", miTupla[:-2])
+print("\33[0mElemento 0:\33[34m", miTupla[0])
+print("\33[0mElemento 1:\33[34m", miTupla[1])
+print("\33[0mElemento despues de 1:\33[34m", miTupla[1:])
+print("\33[0mElementos desde la pos -2 \33[34m", miTupla[:-2])
 
-print("\nTodos los elementos")
+print("\33[0m\nTodos los elementos:\33[33m")
 for elemento in miTupla:
     print(elemento)
 
 t1 = miTupla + (1000, 10000)
 t2 = miTupla * 3
-print("Tupla 1:", t1)
-print("Longitud t1:", len(t1))
-print("Tupla 2:", t2)
-print("Longitud t2:", len(t2))
+print("\33[0mTupla 1:\33[33m", t1)
+print("\33[0mLongitud t1:\33[35m", len(t1))
+print("\33[0mTupla 2:\33[33m", t2)
+print("\33[0mLongitud t2:\33[35m", len(t2))
+print('\33[32m')
 print(10 in miTupla)
 print(-10 not in miTupla)
 
 miTuplaFloat = (1.3, 1.5, 1.8)
-print("Tupla flotante:", miTuplaFloat)
+print("\33[0mTupla flotante:\33[33m", miTuplaFloat)
 
 
 def funcionTupla(estaTupla):
     """Datos de la tupla"""
-    print("Desde el método")
-    print("Elemento 0: ", estaTupla[0])
-    print("Elemento 1: ", estaTupla[1])
-    print("Elemento despues de 1: ", estaTupla[1:])
-    print("Elementos desde la pos -2 ", estaTupla[:-2])
+    print("\33[0mDesde el método")
+    print("\33[0mElemento 0:\33[31m", estaTupla[0])
+    print("\33[0mElemento 1:\33[31m", estaTupla[1])
+    print("\33[0mElemento despues de 1:\33[31m", estaTupla[1:])
+    print("\33[0mElementos desde la pos -2 \33[31m", estaTupla[:-2])
 
 
 funcionTupla(t1)
+print("\33[0m")
