f9b51bdc4c260000c0816384d3280c8ec4ee5c67
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-05 17:15:51 -0500

    Implementar colores

--- Unidad 1/Programa1.py
+++ Unidad 1/Programa1.py
@@ -1,39 +1,40 @@
 def capturaCalificaciones():
-    print("Capturando calificaciones...")
+    print("\33[0mCapturando calificaciones...")
     lista = []
     for i in range(1, 11):
-        calif = int(input("Ingrese calificación " + str(i) + ": "))
+        calif = int(
+            input("\33[0mIngrese calificación " + str(i) + ": \33[33m"))
         lista.append(calif)
     return lista
 
 
 def capturaPrecios():
-    print("Capturando precios...")
+    print("\33[0mCapturando precios...")
     tupla = ()
     for i in range(1, 11):
-        calif = float(input("Ingrese precio " + str(i) + ": "))
+        calif = float(input("\33[0mIngrese precio " + str(i) + ": \33[33m"))
         tupla += (calif,)
     return tupla
 
 
 def procesarLista(lista):
-    print("Procesando lista...")
-    print("Primer elemento:", lista[0])
-    print("Ultimo elemento:", lista[-1])
-    print("Primeros 3 elementos:", lista[0:3])
-    print("Ultimos 3 elementos:", lista[-3:10])
-    print("Elemento mayor:", sorted(lista)[-1])
-    print("Elemento menor:", sorted(lista)[0])
+    print("\33[0mProcesando lista...")
+    print("\33[0mPrimer elemento: \33[32m", lista[0])
+    print("\33[0mUltimo elemento: \33[32m", lista[-1])
+    print("\33[0mPrimeros 3 elementos: \33[32m", lista[0:3])
+    print("\33[0mUltimos 3 elementos: \33[32m", lista[-3:10])
+    print("\33[0mElemento mayor: \33[32m", sorted(lista)[-1])
+    print("\33[0mElemento menor: \33[32m", sorted(lista)[0])
 
 
 def procesarTupla(tupla):
-    print("Procesando tupla...")
-    print("Primer elemento:", tupla[0])
-    print("Ultimo elemento:", tupla[-1])
-    print("Primeros 3 elementos:", tupla[0:3])
-    print("Ultimos 3 elementos:", tupla[-3:10])
-    print("Elemento mayor:", sorted(tupla)[-1])
-    print("Elemento menor:", sorted(tupla)[0])
+    print("\33[0mProcesando tupla...")
+    print("\33[0mPrimer elemento: \33[34m", tupla[0])
+    print("\33[0mUltimo elemento: \33[34m", tupla[-1])
+    print("\33[0mPrimeros 3 elementos: \33[34m", tupla[0:3])
+    print("\33[0mUltimos 3 elementos: \33[34m", tupla[-3:10])
+    print("\33[0mElemento mayor: \33[34m", sorted(tupla)[-1])
+    print("\33[0mElemento menor: \33[34m", sorted(tupla)[0])
 
 
 calificaciones = capturaCalificaciones()
@@ -43,3 +44,5 @@ procesarLista(calificaciones)
 precios = capturaPrecios()
 print(precios)
 procesarTupla(precios)
+
+print("\33[0m")
--- Unidad 1/Programa2.py
+++ Unidad 1/Programa2.py
@@ -1,58 +1,60 @@
 def crearAgenda():
-    print("Creando agenda...")
+    print("\33[0mCreando agenda...")
     agenda = {}
     while True:
         nombre = input(
-            "\nIngrese un nombre, o presione enter para finalizar: ")
+            "\33[0m\nIngrese un nombre, o presione enter para finalizar: \33[33m")
         if nombre == "":
             return agenda
-        telefono = input("Ingrese un número de telefono:")
+        telefono = input("\33[0mIngrese un número de telefono: \33[33m")
         agenda.update({nombre: telefono})
-        print(len(agenda), "elementos creados")
+        print("\33[0m\n", len(agenda), "elementos creados")
 
 
 def mostrarAgenda(agenda):
-    print("Imprimendo agenda...")
+    print("\33[0mImprimendo agenda...")
     for clave in agenda.keys():
-        print("\nNombre:", clave)
-        print("Teléfono:", agenda[clave])
-    print(len(agenda), "elementos mostrados")
-    input("Presione una tecla...")
+        print("\33[0m- - - - - - - - - - -")
+        print("\33[0m\nNombre: \33[32m", clave)
+        print("\33[0mTeléfono: \33[32m", agenda[clave])
+        print("\33[0m- - - - - - - - - - -")
+    print("\33[0m\n", len(agenda), "elementos mostrados")
+    input("\33[0mPresione una tecla...")
 
 
 def editarElemento(agenda):
-    clave = input("Inserte nombre de elemento a editar:")
+    clave = input("\33[0mInserte nombre de elemento a editar: \33[34m")
     if clave in agenda.keys():
-        tel = input("Inserte nuevo teléfono:")
+        tel = input("\33[0mInserte nuevo teléfono: \33[34m")
         agenda[clave] = tel
     else:
-        input("No se encontro clave...")
+        input("\33[0mNo se encontro clave...")
     return agenda
 
 
 def eliminarElemento(agenda):
-    clave = input("Inserte nombre a eliminar a eliminar:")
+    clave = input("\33[0mInserte nombre a eliminar a eliminar: \33[31m")
     if clave in agenda.keys():
         del agenda[clave]
     else:
-        input("No se encontro clave...")
+        input("\33[0mNo se encontro clave...")
     return agenda
 
 
 agenda = {}
 
 while True:
-    print("- - - - - - - - - - -")
-    print("Menú de la agenda")
-    print("- - - - - - - - - - -")
-    print("1. Crear agenda")
-    print("2. Ver agenda")
-    print("3. Editar elemento")
-    print("4. Eliminar elemento")
-    print("- - - - - - - - - - -")
-    print("0. Salir")
-    print("- - - - - - - - - - -")
-    op = int(input("Ingrese una opción: "))
+    print("\33[0m- - - - - - - - - - -")
+    print("\33[0mMenú de la agenda")
+    print("\33[0m- - - - - - - - - - -")
+    print("\33[0m1. Crear agenda")
+    print("\33[0m2. Ver agenda")
+    print("\33[0m3. Editar elemento")
+    print("\33[0m4. Eliminar elemento")
+    print("\33[0m- - - - - - - - - - -")
+    print("\33[0m0. Salir")
+    print("\33[0m- - - - - - - - - - -")
+    op = int(input("\33[0mIngrese una opción: \33[35m"))
     if op == 0:
         break
     elif op == 1:
@@ -63,3 +65,5 @@ while True:
         agenda = editarElemento(agenda)
     elif op == 4:
         agenda = eliminarElemento(agenda)
+
+print("\33[0m")
