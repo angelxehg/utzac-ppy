47f5d5eb427dfec704db8cd9d1f73ef3c29e9b4e
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-17 21:10:46 -0500

    programa

--- Unidad 2/Módulo 5/Sección 5/lab.py
+++ Unidad 2/Módulo 5/Sección 5/lab.py
@@ -1,7 +1,18 @@
 def readint(prompt, min, max):
-    #
-    # tu codigo aqui
-    #
+    while True:
+        try:
+            num = int(input(prompt))
+        except:
+            print("Error: entrada incorrecta")
+            continue
+        try:
+            assert num >= min
+            assert num <= max
+            return num
+        except:
+            print(
+                "Error: el valor no está dentro del rango permitido (" + str(min) + ", " + str(max) + ")")
+            continue
 
 
 v = readint("Ingresa un numero de -10 a 10: ", -10, 10)
