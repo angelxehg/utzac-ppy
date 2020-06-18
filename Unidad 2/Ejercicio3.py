21db3676f531b0a595f90a2c6e8ad1ca30d313fd
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-18 16:20:59 -0500

    Ejercicio 3 completo

---
+++ Unidad 2/Ejercicio3.py
@@ -0,0 +1,4 @@
+cadena = input("\33[0mIngrese la cadena a separar: \33[34m")
+separador = input("\33[0mIngrese el carácter espaciador: \33[34m")[0]
+print("\33[0m")
+print("Resultado:\33[33m", cadena.replace(' ', separador), "\33[0m")
