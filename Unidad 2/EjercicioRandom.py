11573ec0905e877c7162fcd5e1cedc6deca0ebf5
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-19 15:17:47 -0500

    Ejercicio Dado

---
+++ Unidad 2/EjercicioRandom.py
@@ -0,0 +1,18 @@
+from random import randrange
+
+
+def roll():
+    """ Juego Dado """
+    x = randrange(1, 6)
+    y = randrange(1, 6)
+    print("\33[0mJugador 1:\33[33m", x, "\33[0m")
+    print("\33[0mJugador 2:\33[33m", y, "\33[0m")
+    if x < y:
+        print("\33[32mGanó el jugador 1\33[0m")
+    elif y < x:
+        print("\33[32mGanó el jugador 2\33[0m")
+    else:
+        print("\33[32mNo ganó nadie\33[0m")
+
+
+roll()
