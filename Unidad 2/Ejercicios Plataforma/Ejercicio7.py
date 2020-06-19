141e982df2174d25c4ecf9e138a336802386cecd
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-19 16:26:19 -0500

    Solo son dos ejercicios

--- Unidad 2/Ejercicios Plataforma/Ejercicio7.py
+++
@@ -1,3 +0,0 @@
-cadena = input("\33[0mIngrese una frase con espacios: \33[34m")
-letras = [palabra[0] for palabra in cadena.split()]
-print("\33[0mPrimera letra de cada palabra :\33[33m", letras, "\33[0m")
